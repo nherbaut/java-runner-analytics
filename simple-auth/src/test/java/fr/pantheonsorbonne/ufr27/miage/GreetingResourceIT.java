6712ee0f1cd7b95580459b4e5e7c18121cec5198
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2023-08-30 16:00:42 +0200

    no tests yet for simple auth

--- simple-auth/src/test/java/fr/pantheonsorbonne/ufr27/miage/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package fr.pantheonsorbonne.ufr27.miage;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
