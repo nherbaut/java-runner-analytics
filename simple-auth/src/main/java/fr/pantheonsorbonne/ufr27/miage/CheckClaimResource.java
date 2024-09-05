aedabdcf2588ada4ec57e4cabbc593545847edc9
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2024-09-05 15:53:39 +0200

    updated to java 21 provide userinfo in claim check

--- simple-auth/src/main/java/fr/pantheonsorbonne/ufr27/miage/CheckClaimResource.java
+++ simple-auth/src/main/java/fr/pantheonsorbonne/ufr27/miage/CheckClaimResource.java
@@ -2,7 +2,9 @@ package fr.pantheonsorbonne.ufr27.miage;
 
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
+import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HEAD;
 import jakarta.ws.rs.POST;
@@ -10,6 +12,9 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import java.util.stream.Collectors;
 
 @Path("claims-check")
 public class CheckClaimResource {
@@ -18,10 +23,17 @@ public class CheckClaimResource {
         public static native TemplateInstance root();
     }
 
-    @RolesAllowed({"discord-auth", "recaptcha-cleared"})
-    @HEAD
-    public Response getRoles(@Context SecurityContext context) {
-        return Response.status(Response.Status.OK).build();
+    @Inject
+    protected JsonWebToken jwt;
+
+    @PermitAll
+    @GET
+    public Response getUserInfo(@Context SecurityContext context) {
+        Response.ResponseBuilder builder = Response.ok();
+        builder = builder.header("preferred_name", context.getUserPrincipal().getName());
+        builder = builder.header("groups", jwt.getGroups().stream().collect(Collectors.joining(",")));
+        return builder.build();
     }
+
 
 }
